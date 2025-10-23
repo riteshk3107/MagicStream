4c1d9d0af97ab2010badee402bedbafd0a73c0bb
riteshk3107 <riteshpatilmework@gmail.com>
2025-10-23 16:00:33 +0530

    project creation

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	//This is the main function
+
+	router := gin.Default()
+
+	router.GET("/hello", func(c *gin.Context) {
+		c.String(200, "hello in Magic stream")
+
+	})
+
+	if err := router.Run(":8080"); err != nil {
+		fmt.Println("Failed to start server", err)
+	}
+}
